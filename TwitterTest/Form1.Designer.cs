e7517726a23bc32d52ff60cc1303400caf35da8d
Esteban <estebanasla@gmail.com>
2014-04-09 15:34:22 -0300

    Add Test application.

---
+++ TwitterTest/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace TwitterTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxToken = new System.Windows.Forms.TextBox();
+            this.textBoxSToken = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelCharCount = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxToken
+            // 
+            this.textBoxToken.Location = new System.Drawing.Point(88, 12);
+            this.textBoxToken.Name = "textBoxToken";
+            this.textBoxToken.Size = new System.Drawing.Size(446, 20);
+            this.textBoxToken.TabIndex = 0;
+            // 
+            // textBoxSToken
+            // 
+            this.textBoxSToken.Location = new System.Drawing.Point(88, 38);
+            this.textBoxSToken.Name = "textBoxSToken";
+            this.textBoxSToken.Size = new System.Drawing.Size(446, 20);
+            this.textBoxSToken.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "token:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "secretToken:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(88, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(446, 96);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "text:";
+            // 
+            // labelCharCount
+            // 
+            this.labelCharCount.AutoSize = true;
+            this.labelCharCount.Location = new System.Drawing.Point(85, 163);
+            this.labelCharCount.Name = "labelCharCount";
+            this.labelCharCount.Size = new System.Drawing.Size(46, 13);
+            this.labelCharCount.TabIndex = 7;
+            this.labelCharCount.Text = "Chars: 0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(459, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Tweet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(459, 529);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(88, 214);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(446, 309);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 347);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Tweets:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 555);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelCharCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSToken);
+            this.Controls.Add(this.textBoxToken);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxToken;
+        private System.Windows.Forms.TextBox textBoxSToken;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelCharCount;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TwitterTest/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Twitterizer;
+
+namespace TwitterTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static OAuthTokens GetTwitterAuthTokens(string accessToken, string secretToken)
+        {
+            OAuthTokens authorizationToken = new OAuthTokens();
+            authorizationToken.ConsumerKey = "YhCC4XvUipgqBhEWc5DQ";
+            authorizationToken.ConsumerSecret = "j8unuRNhwZdGaQ4Y0mRWZUzqf9BeZZ3aYgGyLsAURAk";
+            authorizationToken.AccessToken = accessToken;
+            authorizationToken.AccessTokenSecret = secretToken;
+            return authorizationToken;
+        }
+
+        public static void Twittear(string token, string secretToken, string tweet)
+        {
+            OAuthTokens oAuthToken = GetTwitterAuthTokens(token, secretToken);
+            Twitterizer.TwitterResponse<TwitterStatus> tweetResponse = TwitterStatus.Update(oAuthToken, tweet);
+            if (tweetResponse.Result != RequestResult.Success)
+            {
+                if (tweetResponse.Result == RequestResult.Unauthorized)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxToken.Text = "145189930-CzJEX2nYihjL3AqeXiRhT86Oy9SyYoSUCzrBG8xT";
+            textBoxSToken.Text = "i7WX3EcgrRwskszSk3xw5Z7uTp22jUfsKYB2gi91U0";
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            labelCharCount.Text = "Chars: " + richTextBox1.Text.Length.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Twittear(textBoxToken.Text, textBoxSToken.Text, richTextBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
